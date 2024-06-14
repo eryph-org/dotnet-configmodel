4d446020ad0d877661caaa02e11e1bfd9347e943
Christopher Mann <christopher.mann@dbosoft.eu>
2024-06-14 15:36:47 +0200

    Code cleanup and use GetEnumProperty

--- src/Eryph.ConfigModel.Catlets/Catlets/Converters/CatletNetworkAdapterConfigConverter.cs
+++ src/Eryph.ConfigModel.Catlets/Catlets/Converters/CatletNetworkAdapterConfigConverter.cs
@@ -2,33 +2,23 @@
 using System.Collections.Generic;
 using Eryph.ConfigModel.Converters;
 
-namespace Eryph.ConfigModel.Catlets.Converters
+namespace Eryph.ConfigModel.Catlets.Converters;
+
+public class CatletNetworkAdapterConfigConverter : DictionaryConverterBase<CatletNetworkAdapterConfig, CatletConfig>
 {
-    public class CatletNetworkAdapterConfigConverter : DictionaryConverterBase<CatletNetworkAdapterConfig, CatletConfig>
+    public class List() : DictionaryToListConverter<CatletNetworkAdapterConfig[], CatletConfig>(
+        nameof(CatletConfig.NetworkAdapters));
+
+    public override CatletNetworkAdapterConfig ConvertFromDictionary(
+        IConverterContext<CatletConfig> context, 
+        IDictionary<object, object> dictionary,
+        object? data = null)
     {
-        public class List : DictionaryToListConverter<CatletNetworkAdapterConfig[], CatletConfig>
+        return new CatletNetworkAdapterConfig
         {
-            public List() : base(nameof(CatletConfig.NetworkAdapters))
-            {
-            }
-        }
-        public override CatletNetworkAdapterConfig ConvertFromDictionary(IConverterContext<CatletConfig> context, 
-            IDictionary<object, object> dictionary, object? data = null)
-        {
-            var mutationString = GetStringProperty(dictionary, nameof(CatletNetworkAdapterConfig.Mutation));
-            MutationType? mutation = null;
-            if (mutationString != null)
-            {
-                if (Enum.TryParse(mutationString, true, out MutationType mutationOut))
-                    mutation = mutationOut;
-            }
-            return new CatletNetworkAdapterConfig
-            {
-                Name = GetStringProperty(dictionary, nameof(CatletNetworkAdapterConfig.Name)),
-                MacAddress = GetStringProperty(dictionary, nameof(CatletNetworkAdapterConfig.MacAddress)),
-                Mutation = mutation
-            };
-        }
+            Name = GetStringProperty(dictionary, nameof(CatletNetworkAdapterConfig.Name)),
+            MacAddress = GetStringProperty(dictionary, nameof(CatletNetworkAdapterConfig.MacAddress)),
+            Mutation = GetEnumProperty<MutationType>(dictionary, nameof(CatletNetworkAdapterConfig.Mutation))
+        };
     }
-    
 }
--- src/Eryph.ConfigModel.Catlets/Catlets/Converters/StrictCatletCapabilityConfigConverter.cs
+++ src/Eryph.ConfigModel.Catlets/Catlets/Converters/StrictCatletCapabilityConfigConverter.cs
@@ -2,47 +2,29 @@
 using System.Collections.Generic;
 using Eryph.ConfigModel.Converters;
 
-namespace Eryph.ConfigModel.Catlets.Converters
+namespace Eryph.ConfigModel.Catlets.Converters;
+
+public class StrictCatletCapabilityConfigConverter : DictionaryConverterBase<CatletCapabilityConfig, CatletConfig>
 {
-    public class StrictCatletCapabilityConfigConverter : DictionaryConverterBase<CatletCapabilityConfig, CatletConfig>
+    public class List() : DictionaryToListConverter<CatletCapabilityConfig[], CatletConfig>(
+        nameof(CatletConfig.Capabilities));
+        
+    public override CatletCapabilityConfig? ConvertFromDictionary(IConverterContext<CatletConfig> context, 
+        IDictionary<object, object> dictionary, object? data = null)
     {
-        public class List : DictionaryToListConverter<CatletCapabilityConfig[], CatletConfig>
-        {
-            public List() : base(nameof(CatletConfig.Capabilities))
-            {
-            }
-        }
+        return ConvertCapabilityConfigConfig(dictionary);
+    }
         
-        public override CatletCapabilityConfig? ConvertFromDictionary(IConverterContext<CatletConfig> context, 
-            IDictionary<object, object> dictionary, object? data = null)
-        {
-            return ConvertCapabilityConfigConfig(dictionary);
-        }
-        
-        protected virtual CatletCapabilityConfig? ConvertCapabilityConfigConfig(object configObject)
-        {
-            if (configObject is IDictionary<object, object> dictionary)
-            {
-                var mutationString = GetStringProperty(dictionary, nameof(CatletCapabilityConfig.Mutation));
-                MutationType? mutation = null;
-                if (mutationString != null)
-                {
-                    if (Enum.TryParse(mutationString, true, out MutationType mutationOut))
-                        mutation = mutationOut;
-                }
-                
-                return new CatletCapabilityConfig
-                {
-                    Name = GetStringProperty(dictionary, nameof(CatletCapabilityConfig.Name)),
-                    Details = GetListProperty<string>(dictionary, nameof(CatletCapabilityConfig.Details)),
-                    Mutation = mutation
-                };
-
-            }
-
+    protected virtual CatletCapabilityConfig? ConvertCapabilityConfigConfig(object configObject)
+    {
+        if (configObject is not IDictionary<object, object> dictionary)
             throw new InvalidConfigModelException();
-
-        }
-
+        
+        return new CatletCapabilityConfig
+        {
+            Name = GetStringProperty(dictionary, nameof(CatletCapabilityConfig.Name)),
+            Details = GetListProperty<string>(dictionary, nameof(CatletCapabilityConfig.Details)),
+            Mutation = GetEnumProperty<MutationType>(dictionary, nameof(CatletCapabilityConfig.Mutation))
+        };
     }
 }
