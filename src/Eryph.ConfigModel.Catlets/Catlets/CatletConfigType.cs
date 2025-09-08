224c5bfb0b71d14e46ef9d16b9f7bac03aa8e2aa
Christopher Mann <christopher.mann@dbosoft.eu>
2025-09-08 14:18:51 +0200

    Improve naming

--- src/Eryph.ConfigModel.Catlets/Catlets/CatletConfigType.cs
+++ src/Eryph.ConfigModel.Catlets/Catlets/CatletConfigType.cs
@@ -2,7 +2,23 @@
 
 public enum CatletConfigType
 {
-    Specification = 0,
-    Resolved = 1,
+    /// <summary>
+    /// This configuration is a normal catlet configuration.
+    /// It can contain unresolved genes.
+    /// User-provided configurations have this type by default.
+    /// </summary>
+    Configuration = 0,
+
+    /// <summary>
+    /// This configuration belongs to a catlet specification. It
+    /// contains only resolved genes and has been normalized.
+    /// </summary>
+    Specification = 1,
+    
+    /// <summary>
+    /// This configuration describes a concrete instance of a catlet.
+    /// It contains information specific to the instance:
+    /// MAC addresses, storage locations, etc.
+    /// </summary>
     Instance = 2,
 }
